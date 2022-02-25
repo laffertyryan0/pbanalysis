30da56e1e8f4caf21f75260c6ec6befaa8cc5453
Lafferty <laffertyrm@nih.gov>
2022-02-24 20:32:23 -0500

    added multinomial and more test code. multinomial needs more testing still

--- R/pbanalysis.r
+++ R/pbanalysis.r
@@ -14,7 +14,7 @@ pb.fit <- function(formula,                    #y~x formula including model and 
                                                #otherwise, use PPO model with nominal = prop.odds.fail variables
                    ){
   #number of places to round output to
-  round_places = 2
+  round_places = 10
 
   #number of samples in dataset
   nsamp = nrow(data)
@@ -235,7 +235,7 @@ pb.fit <- function(formula,                    #y~x formula including model and 
     for(j in 1:nsamp){
       d.S.d.theta = d.S.d.theta +
         w[j]*deltaR0[j]*d.phat.d.theta[j,,]%*%
-        MASS::ginv(diag(phat[j,])-phat[j,]%*%t(phat[j,]))%*%
+        MASS::ginv(diag(phat[j,],names=F)-phat[j,]%*%t(phat[j,]))%*%
         t(d.phat.d.theta[j,,])
     }
 
@@ -244,7 +244,7 @@ pb.fit <- function(formula,                    #y~x formula including model and 
     w.d.S.d.w = array(0,c(nsamp,num_params,T-1))
     for(j in 1:nsamp){
       d.S.d.w[j,,] = deltaR0[j]*
-        d.phat.d.theta[j,,]%*%MASS::ginv(diag(phat[j,])-phat[j,]%*%t(phat[j,]))%*%
+        d.phat.d.theta[j,,]%*%MASS::ginv(diag(phat[j,],names=F)-phat[j,]%*%t(phat[j,]))%*%
         (y_ind[j,] - phat[j,])
       w.d.S.d.w[j,,] = w[j]*d.S.d.w[j,,]
     }
@@ -267,6 +267,93 @@ pb.fit <- function(formula,                    #y~x formula including model and 
   }
   else if(family == "multinomial"){
     # DO MULTINOMIAL MODEL HERE
+
+    #define a factor version of y
+    #if the levels are numbers, relevel so that the highest level is baseline
+    factor_y = as.factor(y)
+    if(is.numeric(y)){
+      factor_y = relevel(factor_y,max(as.numeric(levels(factor_y))))
+    }
+
+    #fit a po model on race 0, that is, data where deltaR0==1
+    #requires ordinal package
+    options(warn=-1)
+    mod = nnet::multinom(factor_y[deltaR0] ~ cov.x[deltaR0,],
+                         weights = w[deltaR0])
+    options(warn=0)
+
+    #define T = number of levels of ordinal response variable
+    T = nlevels(as.factor(y))
+
+    #define s = number of x covariates, r = number of z covariates, p = r + s
+    s = ncol(cov.x)
+    p = 0 + s
+
+    #size of theta vector
+    num_params = (T-1)*(1+p)
+
+    #extract model coefficients
+    #beta0 is the vector of intercepts for each T value
+    #beta is the matrix where each row is a T-value and columns are
+    #associated to covariates
+    coefficients = coef(mod)
+    coefficients = matrix(data = coefficients, nrow = T-1)
+    beta0 = coefficients[,1]
+    beta = coefficients[,-1]
+    beta = matrix(data = beta, nrow = T-1)
+
+    #define the probability estimates for each category
+    #phat[j,t] = race-0 based estimate for probability of category t
+    phat.top = sapply(1:(T-1), function(t) exp(beta0[t] + beta[t,]%*%t(cov.x)))
+    phat = phat.top/(1+rowSums(phat.top))
+
+
+    #define the derivative of phat with respect to parameter vector theta = (beta0,beta)
+    #see ppo model page 609. Possible typo in paper? Using own derivation instead.
+    #the names=F argument in diag just makes sure we get a 1x1 matrix output in case T=2
+
+    d.phat.d.theta = array(0,c(nsamp,num_params,T-1))
+    for(i in 1:nsamp){
+      d.phat.d.theta[i,,] = kronecker((diag(phat[i,],names=F)-phat[i,]%*%t(phat[i,])),
+                                      c(1,cov.x[i,]))
+    }
+
+    #Define d.S.d.theta = derivative of estimating equations with respect to theta
+    d.S.d.theta = array(0,c(num_params,num_params))
+    for(j in 1:nsamp){
+      d.S.d.theta = d.S.d.theta +
+        w[j]*deltaR0[j]*d.phat.d.theta[j,,]%*%
+        MASS::ginv(diag(phat[j,],names=F)-phat[j,]%*%t(phat[j,]))%*%
+        t(d.phat.d.theta[j,,])
+    }
+
+    #Define d.S.d.w = derivative of est. equations with respect to weight
+    d.S.d.w = array(0,c(nsamp,num_params,T-1))
+    w.d.S.d.w = array(0,c(nsamp,num_params,T-1))
+    for(j in 1:nsamp){
+      d.S.d.w[j,,] = deltaR0[j]*
+        d.phat.d.theta[j,,]%*%MASS::ginv(diag(phat[j,],names=F)-phat[j,]%*%t(phat[j,]))%*%
+        (y_ind[j,] - phat[j,])
+      w.d.S.d.w[j,,] = w[j]*d.S.d.w[j,,]
+    }
+
+    #compute d.theta.d.w = (dSdtheta)^-1 * dS.dw
+    d.theta.d.w = array(0,c(nsamp,num_params,T-1))
+    w.d.theta.d.w = array(0,c(nsamp,num_params,T-1))
+
+    for(t in 1:(T-1)){
+      d.theta.d.w[,,t] = d.S.d.w[,,t]%*%MASS::ginv(d.S.d.theta)
+      w.d.theta.d.w[,,t] = w.d.S.d.w[,,t]%*%MASS::ginv(d.S.d.theta)
+    }
+
+
+    #compute d.phat.d.w = d.phat.d.theta times d.theta.d.w
+    d.phat.d.w = array(0,c(nsamp,nsamp,T-1))
+    for(t in 1:(T-1)){
+      d.phat.d.w[,,t] = d.phat.d.theta[,,t]%*%t(d.theta.d.w[,,t])
+    }
+
+
   }
   else if(family == "gaussian"){
     # DO LINEAR MODEL HERE
@@ -426,16 +513,28 @@ pb.fit <- function(formula,                    #y~x formula including model and 
     rownames(unexp.disp) = "result"
   }
 
-  return(list(percent.unexplained = pct.unexp,
+
+  if(family == "gaussian"){
+    sample.sizes = table(data[disparity.group][,])
+  }else{
+    sample.sizes = table(unname(y),data[disparity.group][,])
+  }
+
+  return(list(
+              sample.sizes = sample.sizes,
+#uncomment the following lines to include observed and predicted prevalence in outputs
+#             observed = y,
+#             predicted = phat,
+              percent.unexplained = pct.unexp,
               unexplained.disp = unexp.disp,
-              unexp.disp.variance = variances,
-              sample.sizes = table(unname(y),data[disparity.group][,])))
+              unexp.disp.variance = variances)
+)
 
 }
 
 
 
-#test code
+#test code for multinomial
 
 
 setwd(r"(C:\Users\laffertyrm\Documents\work\PB)")
@@ -451,6 +550,49 @@ out = pb.fit(bmi_cat ~ age + age_square + pir + insurance + phy.act + alc.consum
        majority.group = "white",
        minority.group = c("black","other"),
        prop.odds.fail = NULL, #c("phy.act","alc.consump","smoke2","smoke3"),
-       family = "ordinal")
+       family = "multinomial")
+
+print(out)
+
+
+#test code for linear
+
+
+setwd(r"(C:\Users\laffertyrm\Documents\work\PB)")
+data = read.csv("bmi.csv")
+data$race = array("other",c(nrow(data)))
+data$race[data$deltaR0==1] = "white"
+data$race[data$deltaR1==1] = "black"
+
+out = pb.fit(bmi ~ age + age_square + pir + insurance + phy.act + alc.consump + smoke2 + smoke3,
+             data = data,
+             weights = data$sample_weight,
+             disparity.group = "race",
+             majority.group = "white",
+             minority.group = c("black","other"),
+             prop.odds.fail = NULL, #c("phy.act","alc.consump","smoke2","smoke3"),
+             family = "gaussian")
+
+print(out)
+
+
+#test code for binary logistic
+
+
+setwd(r"(C:\Users\laffertyrm\Documents\work\PB)")
+data = read.csv("bmi_cat.csv")
+data$race = array("other",c(nrow(data)))
+data$race[data$deltaR0==1] = "white"
+data$race[data$deltaR1==1] = "black"
+data$bmi_cat[data$bmi_cat == 3] = 2
+
+out = pb.fit(bmi_cat ~ age + age_square + pir + insurance + phy.act + alc.consump + smoke2 + smoke3,
+             data = data,
+             weights = data$sample_weight,
+             disparity.group = "race",
+             majority.group = "white",
+             minority.group = c("black","other"),
+             prop.odds.fail = NULL, #c("phy.act","alc.consump","smoke2","smoke3"),
+             family = "multinomial")
 
 print(out)
