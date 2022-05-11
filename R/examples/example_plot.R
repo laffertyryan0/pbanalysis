bbdc96853dfe4b54f6c2b5805ed40048ada2b1cb
Lafferty <laffertyrm@nih.gov>
2022-05-11 10:53:35 -0400

    plots and documentation updates

---
+++ R/examples/example_plot.R
@@ -0,0 +1,19 @@
+#create plots for ordinal
+
+
+data = data.frame(bmi_cat = NHANES::NHANES$BMI_WHO,
+                  race = NHANES::NHANES$Race1,
+                  age = NHANES::NHANES$Age,
+                  poverty = NHANES::NHANES$Poverty
+)
+data = na.omit(data)
+
+out = pb.fit(bmi_cat ~ age + poverty,
+             data = data,
+             disparity.group = "race",
+             majority.group = "White",
+             minority.group = c("Black"),
+             prop.odds.fail = c("age"),
+             family = "ordinal")
+
+plot(out)
--- R/examples/examples_2.R
+++ R/examples/examples_2.R
@@ -18,3 +18,44 @@ out = pb.fit(bmi_cat ~ age + poverty,
 
 print(out)
 
+
+#test code for binary outcome
+
+
+data = data.frame(bmi_cat = as.integer(NHANES::NHANES$BMI > 20) ,
+                  race = NHANES::NHANES$Race1,
+                  age = NHANES::NHANES$Age,
+                  poverty = NHANES::NHANES$Poverty
+)
+data = na.omit(data)
+
+out = pb.fit(bmi_cat ~ age + poverty,
+             data = data,
+             disparity.group = "race",
+             majority.group = "White",
+             minority.group = c("Black"),
+             prop.odds.fail = c("age"),
+             family = "multinomial")
+
+print(out)
+
+#test code for continuous outcome
+
+
+data = data.frame(bmi_cat = NHANES::NHANES$BMI ,
+                  race = NHANES::NHANES$Race1,
+                  age = NHANES::NHANES$Age,
+                  poverty = NHANES::NHANES$Poverty
+)
+data = na.omit(data)
+
+out = pb.fit(bmi_cat ~ age + poverty,
+             data = data,
+             disparity.group = "race",
+             majority.group = "White",
+             minority.group = c("Black"),
+             prop.odds.fail = c("age"),
+             family = "gaussian")
+
+print(out)
+
